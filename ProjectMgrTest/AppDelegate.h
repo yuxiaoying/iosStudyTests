1bf65bd4b57f4dc1a6a1aff93be85e78aee8af33
寂惺 <jixing@jixingdemacbook-air.local>
2018-05-29 10:35:24 +0800

    Initial Commit

---
+++ ProjectMgrTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectMgrTest
+//
+//  Created by 寂惺 on 2018/5/29.
+//  Copyright © 2018年 haha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectMgrTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectMgrTest
+//
+//  Created by 寂惺 on 2018/5/29.
+//  Copyright © 2018年 haha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
